13831c17f6649f0859f6f429576f82ae1464ac03
hiEping <hi_eping@qq.com>
2021-07-27 12:40:58 +0800

    add blocks: 分析页,监控页,流程图

--- config/routes.ts
+++ config/routes.ts
@@ -1,30 +1,29 @@
-﻿export default [
+export default [
   {
     path: '/welcome',
     name: 'welcome',
     icon: 'smile',
     component: './Welcome',
-  },
-  // 数据看板
+  }, // 数据看板
   {
-    path:'/dashBoard',
-    name:'dashboard',
-    icon:'icon-dashboard',
-    routes:[
+    path: '/dashBoard',
+    name: 'dashboard',
+    icon: 'icon-dashboard',
+    routes: [
       {
-        path:'/analysis',
-        name:'analysis',
-        component:'./dashBoard/analysis',
+        path: 'analysis',
+        name: 'analysis',
+        component: './dashBoard/analysis',
       },
       {
-        path:'/monitor',
-        name:'monitor',
-        component:'./dashBoard/monitor',
+        path: 'monitor',
+        name: 'monitor',
+        component: './dashBoard/monitor',
       },
       {
-        path:'workplace',
-        name:'workplace',
-        component:'./dashBoard/workplace',
+        path: 'workplace',
+        name: 'workplace',
+        component: './dashBoard/workplace',
       },
     ],
   },
@@ -43,8 +42,7 @@
         ],
       },
     ],
-  },
-    // 管理页
+  }, // 管理页
   {
     path: '/admin',
     name: 'admin',
@@ -73,79 +71,82 @@
         component: './Welcome',
       },
     ],
-  },
-  // 数据查询
+  }, // 数据查询
   {
     path: '/list',
     name: 'list',
     icon: 'table',
     routes: [
       {
-        path:'table-list',
-        name:'table-list',
+        path: 'table-list',
+        name: 'table-list',
         component: './TableList/index',
       },
     ],
-  },
-  // 数据填报
+  }, // 数据填报
   {
-    path:'/data-recording',
-    name:'data-recording',
-    icon:'icon-bi',
-    routes:[
+    path: '/data-recording',
+    name: 'data-recording',
+    icon: 'icon-bi',
+    routes: [
       {
-        path:'proform',
-        name:'proform',
-        component:'./data-recording/proform',
+        path: 'proform',
+        name: 'proform',
+        component: './data-recording/proform',
       },
       {
-        path:'schema-form',
-        name:'schema-form',
-        component:'./data-recording/schema-form',
+        path: 'schema-form',
+        name: 'schema-form',
+        component: './data-recording/schema-form',
       },
     ],
-  },
-  // 资料库
+  }, // 资料库
   {
-    path:'/doc-lib',
-    name:'doc-lib',
-    icon:'icon-dangan',
-    routes:[
+    path: '/doc-lib',
+    name: 'doc-lib',
+    icon: 'icon-dangan',
+    routes: [
       {
-      path:'project',
-      name:'project',
-      component:'./doc-lib/project',
+        path: 'project',
+        name: 'project',
+        component: './doc-lib/project',
       },
       {
-      path:'project-design',
-      name:'project-design',
-      component:'./doc-lib/project-design',
+        path: 'project-design',
+        name: 'project-design',
+        component: './doc-lib/project-design',
       },
       {
-      path:'project-plan',
-      name:'project-plan',
-      component:'./doc-lib/project-plan',
+        path: 'project-plan',
+        name: 'project-plan',
+        component: './doc-lib/project-plan',
       },
       {
-      path:'project-hetong',
-      name:'project-hetong',
-      component:'./doc-lib/project-hetong',
+        path: 'project-hetong',
+        name: 'project-hetong',
+        component: './doc-lib/project-hetong',
       },
       {
-      path:'law',
-      name:'law',
-      component:'./doc-lib/law',
+        path: 'law',
+        name: 'law',
+        component: './doc-lib/law',
       },
       {
-      path:'project-meeting',
-      name:'project-meeting',
-      component:'./doc-lib/project-meeting',
+        path: 'project-meeting',
+        name: 'project-meeting',
+        component: './doc-lib/project-meeting',
       },
     ],
   },
   {
     path: '/',
     redirect: '/welcome',
+  },
+  {
+    name: 'flow',
+    icon: 'smile',
+    path: '/flow',
+    component: './flow',
   },
   {
     component: './404',
